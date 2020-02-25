324ca98ff2e71539f35f0132aa53a938c491969a
Nick Solonyy <nicksolony@gmail.com>
2020-02-25 06:23:05 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,18 @@
-class Model
+class Dog
+
+  @@all =[]
+#  attr_reader :name, :breed, :age
+binding.pry
+def initialize(name)
+  
+  @name = name
+
+    @@all < self
+  end
+
+  def self.all
+    @@all
+  end
+
 
 end
